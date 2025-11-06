05f68b975a48d19c05248ed50d0b851568ed53f7
AhmetFatihGuzeller <ahmetguzeller@arkadyas.com>
2025-11-06 16:25:10 +0300

    mail kaydı kaldırıldı.

--- Norosirurjihemsireligi2026_Web/tr/Default.aspx.cs
+++ Norosirurjihemsireligi2026_Web/tr/Default.aspx.cs
@@ -29,7 +29,6 @@ namespace Norosirurjihemsireligi2026_Web.tr
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            new MailGonderimIslemleri().KayitBilgilendirme("NRSH2025110615065381", "tr");
 
 
             if (!IsPostBack)
